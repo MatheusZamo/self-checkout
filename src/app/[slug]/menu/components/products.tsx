eda9a8abcf568525144b898edc7fe8a36b20aa7d
MatheusZamo <19matheushenrique03@gmail.com>
2025-02-21 11:26:21 -0300

    feat: add product header component

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,28 +1,31 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 interface ProductsProps {
     products: Product[]
 }
 
 const Products = ({products} : ProductsProps) => {
+    const {slug} = useParams<{ slug: string }>();
+
     return (<div className="space-y-3 px-5">
-        {products.map(products => (
-            <Link key={products.id} 
-            className='flex items-center justify-between gap-10 py-3 border-b' href={'/'}>
+        {products.map(product => (
+            <Link key={product.id} 
+            className='flex items-center justify-between gap-10 py-3 border-b' href={`/${slug}/menu/${product.id}`}>
                 <div>
-                    <h3 className="text-sm font-medium">{products.name}</h3>
-                    <p className="line-clamp-2 text-muted-foreground"> {products.description}</p>
+                    <h3 className="text-sm font-medium">{product.name}</h3>
+                    <p className="line-clamp-2 text-muted-foreground"> {product.description}</p>
                     <p className='pt-3 text-sm font-semibold'>
                         {new Intl.NumberFormat('pt-BR',{
                             style : 'currency',
                             currency:'BRL',
-                        }).format(products.price)}
+                        }).format(product.price)}
                     </p>
                 </div>
                 <div className="relative min-h-[82px] min-w-[120px]">
-                    <Image src={products.imageUrl} alt={products.name} fill className='rounded-lg object-contain' />
+                    <Image src={product.imageUrl} alt={product.name} fill className='rounded-lg object-contain' />
                 </div>
             </Link>
         ))}
