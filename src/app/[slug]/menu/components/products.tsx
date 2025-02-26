f79d041ae3772ca9299d10b65fc2ce56e5de54ff
MatheusZamo <19matheushenrique03@gmail.com>
2025-02-26 18:48:46 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,34 +1,49 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 
 import { formatCurrency } from "@/helpers/format-currency";
 
 interface ProductsProps {
-    products: Product[]
+  products: Product[];
 }
 
-const Products = ({products} : ProductsProps) => {
-    const {slug} = useParams<{ slug: string }>();
+const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  const searchParams = useSearchParams();
+  const consumptionMethod = searchParams.get("consumptionMethod");
 
-    return (<div className="space-y-3 px-5">
-        {products.map(product => (
-            <Link key={product.id} 
-            className='flex items-center justify-between gap-10 py-3 border-b' href={`/${slug}/menu/${product.id}`}>
-                <div>
-                    <h3 className="text-sm font-medium">{product.name}</h3>
-                    <p className="line-clamp-2 text-muted-foreground"> {product.description}</p>
-                    <p className='pt-3 text-sm font-semibold'>
-                        {formatCurrency(product.price)}
-                    </p>
-                </div>
-                <div className="relative min-h-[82px] min-w-[120px]">
-                    <Image src={product.imageUrl} alt={product.name} fill className='rounded-lg object-contain' />
-                </div>
-            </Link>
-        ))}
-    </div> );
-}
- 
+  return (
+    <div className="space-y-3 px-5">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          className="flex items-center justify-between gap-10 border-b py-3"
+          href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`}
+        >
+          <div>
+            <h3 className="text-sm font-medium">{product.name}</h3>
+            <p className="line-clamp-2 text-muted-foreground">
+              {" "}
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}
+            </p>
+          </div>
+          <div className="relative min-h-[82px] min-w-[120px]">
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-contain"
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
 export default Products;
